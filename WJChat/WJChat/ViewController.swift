556ce416ba8f4d71988610c12d2c5000f8966507
NobodyKnowsWang <wangjun995101207@gmail.com>
2019-04-22 22:23:39 +0900

    create

---
+++ WJChat/WJChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WJChat
+//
+//  Created by Jun Wang on 2019/04/22.
+//  Copyright © 2019 Jun Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
